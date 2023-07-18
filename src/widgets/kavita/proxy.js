7eda97418463b095cf0c6730346603eec0959794
Emmanuel Alap <15620712+ealap@users.noreply.github.com>
2023-07-18 16:43:23 +0200

    feat(fork): Apply custom styling
    Normalize header name
    
    Signed-off-by: Emmanuel Alap <15620712+ealap@users.noreply.github.com>

--- src/widgets/kavita/proxy.js
+++ src/widgets/kavita/proxy.js
@@ -37,7 +37,7 @@ async function login(widget, service) {
 async function apiCall(widget, endpoint, service) {
   const key = `${sessionTokenCacheKey}.${service}`;
   const headers = {
-    "content-type": "application/json",
+    "Content-Type": "application/json",
     "Authorization": `Bearer ${cache.get(key)}`,
   }
 
